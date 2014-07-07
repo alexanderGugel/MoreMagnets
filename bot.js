4b28c8313ae937e80e94962cdaad58d41f2d88d7
Alexander Gugel <alexander.gugel@gmail.com>
2014-07-07 15:25:54 -0700

    Implement basic bot

--- bot.js
+++ bot.js
@@ -12,6 +12,24 @@ dht.listen(dhtPort, function () {
   console.info('Bot started listening on port ' + dhtPort);
 });
 
+dht.on('peer', function (addr, infoHash, from) {
+  console.info('Found potential peer ' + addr + ' through ' + from);
+  // Store peers.
+  // get id for result
+  // Score: FIRST time peer has been added
+  redis.zrank('m:' + infoHash + ':p', addr, function (err, exists) {
+    if (!exists) {
+      redis.zadd('m:' + infoHash + ':p', new Date().getTime(), addr);
+    }
+  });
+});
+
+// Used for building the graph.
+dht.on('node', function (addr, nodeId, from) {
+  console.info('Found potential node ' + addr + ' through ' + from);
+  redis.sadd('edges', from + '-' + addr);
+});
+
 var crawl = function (infoHash, callback) {
   console.info('Crawling ' + infoHash);
   // dht.lookup('CE9FBDAA734CFBC160E8EF9D29072646C09958DD');
@@ -33,15 +51,14 @@ var crawl = function (infoHash, callback) {
 };
 
 var crawlNext = function () {
+  // Heads up! This seems to be blocking!
   redis.lpop('m:crawl', function (err, infoHash) {
     if (err) {
       console.error('Failed to retrieve crawl job: ' + err.message);
     } else if (infoHash) {
       // Emulate ring buffer.
       redis.rpush('m:crawl', infoHash);
-      crawl(infoHash, function (err, data) {
-        console.log(data);
-      });
+      crawl(infoHash);
     }
   });
 };
@@ -53,10 +70,4 @@ dht.on('ready', function () {
   crawlNext();
   // Retrieve and crawl next Magnet URI every 10 seconds.
   setInterval(crawlNext, 10000);
-});
-
-var i = 0;
-
-dht.on('peer', function (addr, hash, from) {
-  console.info(++i, 'found potential peer ' + addr + ' using ' + from);
 });
