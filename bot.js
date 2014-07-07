8905bcb63010ff8b35275786b8eed8fcc6638ee6
Alexander Gugel <alexander.gugel@gmail.com>
2014-07-07 11:11:52 -0700

    Add comments to bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,17 @@
 // Get redis client.
 var redis = require('./redis');
-var DHT = require('bittorrent-dht');
 
+var DHT = require('bittorrent-dht');
 var dht = new DHT();
 
+// Port 6881 works best.
 var dhtPort = process.env.DHT_PORT || 6881;
+
 dht.listen(dhtPort, function () {
   console.log('now listening');
 });
 
+// Don't lookup before dht is ready (started listening).
 dht.on('ready', function () {
   dht.lookup('CE9FBDAA734CFBC160E8EF9D29072646C09958DD');
 });
