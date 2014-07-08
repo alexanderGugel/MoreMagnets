971dd7f1d7e57bb2a459693f7b7fd76d95dfde81
Alexander Gugel <alexander.gugel@gmail.com>
2014-07-08 13:03:30 -0700

    Optimize crawler

--- crawler.js
+++ crawler.js
@@ -94,7 +94,7 @@ var crawlNext = function () {
 // Don't lookup before dht is ready (started listening).
 dht.on('ready', function () {
   // Retrieve and crawl next Magnet URI every 7 seconds.
-  setInterval(crawlNext, 7000);
+  setInterval(crawlNext, 3000);
   // Invoke crawlNext, since we don't want to wait 10 seconds before we start
   // retrieving the first crawl job.
   crawlNext();
