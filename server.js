46d47404e3ba181bd50e635da5bd0c85f869b292
Alexander Gugel <alexander.gugel@gmail.com>
2014-07-08 14:06:14 -0700

    Fine tune magic numbers in server (status stats)

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ var getStats = function (callback) {
   multi.zcard('loc_stats:regions');
   multi.zcard('loc_stats:cities');
   multi.zrevrange(['loc_stats:lls', 0, 5000, 'WITHSCORES']);
-  multi.zrevrange(['peers', 0, 5000, 'WITHSCORES']);
+  multi.zrevrange(['peers', 0, 1000, 'WITHSCORES']);
 
   multi.exec(function (err, data) {
     var edges = {
