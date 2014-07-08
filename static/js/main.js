96a0233e41c08847b47dbca621b1e98470386540
Alexander Gugel <alexander.gugel@gmail.com>
2014-07-07 22:14:44 -0700

    Display stats

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,8 @@
-var config = {
+Chart.defaults.global.responsive = true;
+Chart.defaults.global.showScale = false;
+Chart.defaults.global.showTooltips = false;
+
+var options = {
 
     ///Boolean - Whether grid lines are shown across the chart
     scaleShowGridLines : false,
@@ -10,16 +14,7 @@ var config = {
     bezierCurveTension : 0.4,
 
     //Boolean - Whether to show a dot for each point
-    pointDot : true,
-
-    //Number - Radius of each point dot in pixels
-    pointDotRadius : 1,
-
-    //Number - Pixel width of point dot stroke
-    pointDotStrokeWidth : 1,
-
-    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-    pointHitDetectionRadius : 20,
+    pointDot : false,
 
     //Boolean - Whether to show a stroke for datasets
     datasetStroke : true,
@@ -28,17 +23,20 @@ var config = {
     datasetStrokeWidth : 1,
 
     //Boolean - Whether to fill the dataset with a colour
-    datasetFill : false,
-
-    //String - A legend template
-    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span> </li><%}%></ul>"
-
+    datasetFill : false
 };
 
 var genData = function (labels, data) {
   return {
     labels: labels,
-    datasets: [{ data: data }]
+    datasets: [{
+      strokeColor: "#eee",
+      data: data,
+      pointColor: "rgba(220,220,220,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(220,220,220,1)",
+    }]
   };
 };
 
@@ -49,14 +47,8 @@ var drawCharts = function () {
     $canvas.attr('width', $canvas.parent().width());
     var ctx = $canvas.get(0).getContext('2d');
     var data = $canvas.data('data').split(',');
-    // var labels = [];
-    //
-    // for (var i = 0; i < data.length; i++) {
-    //   labels.push('');
-    // }
-
     var labels = $canvas.data('labels').split(',');
-    new Chart(ctx).Line(genData(labels, data), config);
+    new Chart(ctx).Line(genData(labels, data), options);
   });
 };
 
